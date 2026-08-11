20db41d18dbd57326811d5c1780cbc4f17c4f40b
xanity <173572284+xanity-07@users.noreply.github.com>
2026-08-11 15:32:39 -0400

    feat: types to mock api some api calls before implementing db connection

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,15 +1,39 @@
 import type { Application } from 'express';
 import express from 'express';
+import { ExpenseCategory, Expenses } from './types/index.js';
 
 const app: Application = express();
 const PORT = 4001;
 
 app.use(express.json());
 
+let fakeExpenses: Expenses[] = [
+    {
+        id: '1',
+        userId: 'user123',
+        amount: 45.99,
+        category: ExpenseCategory.FOOD,
+        description: '',
+        date: new Date('2026-08-01'),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+    {
+        id: '2',
+        userId: 'user123',
+        amount: 31.99,
+        category: ExpenseCategory.SHOPPING,
+        description: '',
+        date: new Date('2026-08-04'),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+];
+
 app.get('/', (req, res) => {
-  res.send('hello from typescript and express');
+    res.send('hello from typescript and express');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on localhost:${PORT}`);
+    console.log(`Server is running on localhost:${PORT}`);
 });
---
+++ apps/backend/src/types/index.ts
@@ -0,0 +1,30 @@
+export enum ExpenseCategory {
+    FOOD = 'food',
+    TRANSPORT = 'transport',
+    UTILITIES = 'utilities',
+    ENTERTAINMENT = 'entertainment',
+    HEALTHCARE = 'healthcare',
+    SHOPPING = 'shopping',
+    EDUCATION = 'education',
+    OTHER = 'other',
+}
+
+export interface Expenses {
+    id: string;
+    userId: string;
+    amount: number;
+    category: ExpenseCategory;
+    description: string;
+    date: Date;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
